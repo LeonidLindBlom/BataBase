30ce45b97e2a40bdeef60e7874932400f5d8cd44
Leonid Galyba <72228673+LeonidLindBlom@users.noreply.github.com>
2024-05-22 17:18:40 +0600

    Update 4.4.sql

--- 4.4.sql
+++ 4.4.sql
@@ -1,20 +1,20 @@
 -- Для каждого сотрудника выведите его идентификатор, время его приема на работу, а также ту же самую информацию для сотрудника, который был принят на работу сразу после него (время приема в один день - приняты в одно и то же время)
-SELECT 
-    e1."BusinessEntityID" AS EmployeeID, 
+select 
+    e1."BusinessEntityID" as EmployeeID, 
     e1."HireDate", 
     (
-        SELECT e2."BusinessEntityID" 
+        select e2."BusinessEntityID" 
         from "HumanResources"."Employee" e2 
-        WHERE e2."HireDate" > e1."HireDate"
+        where e2."HireDate" > e1."HireDate"
         order by e2."HireDate"
         limit 1
-    ) AS NextEmployeeID, 
+    ) as NextEmployeeID, 
     (
-        SELECT e2."HireDate"
-        FROM "HumanResources"."Employee" e2 
-        WHERE e2."HireDate" > e1."HireDate"
+        select e2."HireDate"
+        from "HumanResources"."Employee" e2 
+        where e2."HireDate" > e1."HireDate"
         order by e2."HireDate"
         limit 1
-    ) AS NextEmployeeHireDate
-FROM "HumanResources"."Employee" e1
-ORDER BY e1."HireDate" asc
+    ) as NextEmployeeHireDate
+from "HumanResources"."Employee" e1
+order by e1."HireDate" asc
